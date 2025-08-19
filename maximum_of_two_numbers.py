44a30e736061d18e286fb4a18dbe53b18dd23855
praveentr24cse <praveentr.24cse@cambridge.edu.in>
2025-08-19 07:22:03 +0530

    Update maximum_of_two_numbers.py

--- maximum_of_two_numbers.py
+++ maximum_of_two_numbers.py
@@ -2,7 +2,7 @@
 
 def add(a, b):
     """Adds two numbers and returns the sum."""
-    return a * b
+    return a + b
 
 def subtract(a, b):
     """Subtracts two numbers and returns the difference."""
